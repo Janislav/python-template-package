3bcd88ed341b730a459c9a2563ebff957ad8bc45
Jan Börner <jan.boerner@nexiles.com>
2013-11-27 21:47:28 +0100

    Changed setup.py and added introduction.rst

---
+++ docs/introduction.rst
@@ -0,0 +1,29 @@
+====================
+Project Introduction
+====================
+
+:author: Jan Börner
+:date: today
+
+Motivation
+==========
+
+Creating python modules and packages are daily tasks in the live of a python developer,
+but actually, it's basically everytime the same process.
+
+So i'am thought it would be a good idea to give an example and a template
+which can be used to setup a python module without to have a bigger dive
+into the matter.
+
+
+Things you should notice
+========================
+
+The way this package is build **IS NOT** the default way of doing
+(if their is a way), but it has some advantages in my eyes which i will
+maybe discuse in the coming docs, or not, we will see. Moreover i want to
+say that i'am **NOT** the "expert" in this topic, but i've a working way
+and want to share it ;)
+
+
+.. vim: set ft=rst tw=75 nocin nosi ai spell sw=4 ts=4 expandtab:
--- src/foo.bar/setup.py
+++ src/foo.bar/setup.py
@@ -1,36 +1,13 @@
 # -*- coding: utf-8 -*-
-import os
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = '0.1dev'
-
-long_description = (
-    read('../../README.rst')
-    )
-
-setup(name='foo.bar',
-      version=version,
-      description="foo.bar",
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-      #keywords='',
-      author='John Doe',
-      author_email='jd@example.com',
-      packages=find_packages('.', exclude=['ez_setup']),
-      package_dir={'.': 'foo'},
-      namespace_packages=['foo'],
-      include_package_data=True,
-      zip_safe=True,
-      entry_points={'console_scripts': ["myCommand = foo.bar:main"]},
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        ],
-      )
+setup(
+    name='foo.bar',
+    version='0.1.0',
+    author='J. Random Hacker',
+    author_email='jrh@example.com',
+    packages=['foo', 'foo.bar'],
+    description='A template python package.',
+    long_description=open('../../README.rst').read(),
+    entry_points={'console_scripts': ["myCommand = foo.bar:main"]},
+)
